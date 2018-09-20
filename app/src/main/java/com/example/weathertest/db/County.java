7147dbce0749d9c2f2fad7bef2a699db8d61c86c
DACHUNG <ganguochang@hotmail.com>
2018-09-20 17:42:05 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/weathertest/db/County.java
@@ -0,0 +1,38 @@
+package com.example.weathertest.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+
+    private int id;
+
+    private String contyName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getWeatherId(){
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId){
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId(){
+        return cityId;
+    }
+
+    public void setCityId(int cityId){
+        this.cityId = cityId;
+    }
+}
